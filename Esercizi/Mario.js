df3992ab16f090beb2e454fc8057bf7ac02df068
mariofrezza <mariofrezza@live.it>
2023-09-29 22:07:48 +0200

    qualcosa

--- Esercizi/Mario.js
+++ Esercizi/Mario.js
@@ -1,5 +1,17 @@
 function main() {
-    console.log('ciao sono Mario')
-}
+    console.log("fabrifibra")
+    //adesso parleremo delle variabili
+    /*asasxasda mario 
+    wooooooowwwww okok
+    funge yeahhhhh
+    */
+   const pippo= 10
+   const pomodoro= 22
+   let banana= 20
+   banana=21
 
+   console.log(pippo,pomodoro,banana)
+   console.log (pippo+pomodoro)
+}
+   
 main()
