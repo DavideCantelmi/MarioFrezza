19294fafff55f5549c40e30f8f996c1e30147068
Davide Cantelmi <d.cantelmi@apio.cc>
2023-10-30 19:55:00 +0100

    contaPari

---
+++ Esercizi/contaPari.js
@@ -0,0 +1,11 @@
+function main() {
+    const n = 10 // impostare il valore che più si preferisce
+
+    contatore = 1 
+    while (contatore <= n) {
+        console.log(2*contatore)
+        contatore = contatore + 1
+    }
+}
+
+main()
